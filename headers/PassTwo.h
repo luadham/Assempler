b0f3f3f8d0b7f423f6da2cebe1ab52acd3752e32
luadham <luadham@gmail.com>
2022-01-01 16:40:33 +0200

    Commented

--- headers/PassTwo.h
+++ headers/PassTwo.h
@@ -24,6 +24,9 @@ private:
     Calculator calculator;
     vector<string> objCodes;
 
+    /*
+     * Get Operand Location From symbol table
+     */
     string getOperandLocation(string operand) {
         for (auto i: symbolTable) {
             if (operand == i.codeLine.label) return i.locationLine;
@@ -31,10 +34,17 @@ private:
         return "";
     }
 
+    /*
+     * Check if c is alpha char or not
+     */
     bool isChar(char c) {
         return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
     }
 
+    /*
+     * Generate Obj Code from input
+     * @modify objCode Vector
+     */
     void generateObjCodes() {
         for (int i = 0; i < symbolTable.size() - 1; i++) {
             string operand = symbolTable[i].codeLine.operand;
@@ -67,10 +77,19 @@ private:
         }
     }
 
+    /*
+     * Get Format two Obj code
+     * @return objcode
+     */
     string format_two(string opCode, char r1, char r2) {
         FormatTwo formatTwo;
         return formatTwo.generateObjCode(opCode, r1, r2);
     }
+
+    /*
+     * Get Format Six obj code
+     * @return objCode
+     */
     string format_six(string operand, string opCode) {
         FormatSix formatSix;
         string _obCode = getOpCode(opCode, operand);
@@ -79,6 +98,11 @@ private:
         int x = isIndexed(operand);
         return formatSix.generateObjCode(_obCode, x, address, "1");
     }
+
+    /*
+     * Get Format Five ObjCode
+     * @return objCode
+     */
     string format_five(string operand, string opCode, string pc) {
         FormatFive formatFive;
         string _obCode = getOpCode(opCode, operand);
@@ -91,6 +115,10 @@ private:
         return formatFive.generateObjCode(_obCode, x, b, p, f2, displacement(pc, validOperand));
     }
 
+    /*
+     * Get objCode for format four
+     * @return objCode
+     */
     string format_four(string operand, string opCode) {
         FormatFour formatFour;
         string _opCode = getOpCode(opCode, operand);
@@ -102,6 +130,10 @@ private:
         return formatFour.generateObjCode(_opCode, x, b, p, address);
     }
 
+    /*
+     * Get obj code for format three
+     * @return objCode
+     */
     string format_three(string operand, string opCode, string pc) {
         FormatThree formatThree;
         string _opCode = getOpCode(opCode, operand);
@@ -113,17 +145,30 @@ private:
                                            displacement(pc, validOperand));
     }
 
+    /*
+     * Calculate opCode with n, i
+     * @return new opCode
+     */
     string getOpCode(string oldOpCode, string operand) {
         int opCode = addressingMode(operand) + calculator.fromHexToDecimal(oldOpCode);
         return calculator.fromDecToHex(opCode);
     }
 
+    /*
+     * Just for testing
+     */
     void printObjCodes() {
         for (int i = 0; i < objCodes.size(); i++) {
             cout << objCodes[i] << endl;
         }
     }
 
+    /*
+     * Split string that have ,
+     * ex : adham, adel -> st1 = adham; str2 = adel;
+     * @param str, str1, str2
+     * @modify str1, str2
+     */
     void splitStr(string str, string &reg1, string &reg2) {
         int cnt = 0;
         for (int i = 0; i < str.length(); i++) {
@@ -140,6 +185,11 @@ private:
         reg2.erase(remove(reg2.begin(), reg2.end(), ' '), reg2.end());
     }
 
+    /*
+     * ex: +jsub to jsub
+     *      #param to param
+     * @return string without starting symbol
+     */
     string getRestOfString(string str) {
         if (isChar(str[0])) return str;
         string res = "";
